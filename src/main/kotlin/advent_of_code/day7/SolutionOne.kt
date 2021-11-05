115cd3e1ffc89088c588e436b1ba444369049bf8
Alex Alexeyev <aasmc@yandex.ru>
2021-11-05 22:20:16 +0300

    Solve Advent of Code 2020 day 7 problem

---
+++ src/main/kotlin/advent_of_code/day7/SolutionOne.kt
@@ -0,0 +1,63 @@
+package advent_of_code.day7
+
+import java.io.File
+
+typealias Color = String
+typealias Rule = Set<String>
+
+const val SHINY_GOLD = "shiny gold"
+
+fun main() {
+    val rules: Map<Color, Rule> = buildBagTree()
+    val containers = findContainersDFS(rules)
+    println(containers)
+    println()
+    println(containers.size)
+}
+
+fun findContainersDFS(rules: Map<Color, Rule>): Set<Color> {
+    var known = setOf(SHINY_GOLD)
+    var next = setOf(SHINY_GOLD) + rules[SHINY_GOLD]!!
+    while (true) {
+        val toFind = next - known
+        if (toFind.isEmpty()) break
+        known = known + next
+        next = toFind.mapNotNull { rules[it] }.flatten().toSet()
+    }
+    return known - SHINY_GOLD
+}
+
+/**
+ * Builds a map of color of a bag contained in another bag TO
+ * a set of colors of bags that can potentially contain our bag.
+ */
+fun buildBagTree(): Map<Color, Set<String>> {
+    val rules = hashMapOf<Color, Rule>()
+    File("src/main/kotlin/advent_of_code/day7/input.txt")
+        .forEachLine { line ->
+            val (parent, allChildren) = line
+                .replace(Regex("\\d+"), "")
+                .replace(Regex("bags?\\.?"), "")
+                .split("contain")
+                .map { it.trim() }
+            val childrenColors = allChildren.split(',').map { it.trim() }.toSet()
+
+            for (childColor in childrenColors) {
+                rules.compute(childColor) { _, rule ->
+                    if (rule == null) {
+                        setOf(parent)
+                    } else {
+                        rule + parent
+                    }
+                }
+            }
+        }
+    return rules
+}
+
+
+
+
+
+
+
---
+++ src/main/kotlin/advent_of_code/day7/SolutionTwo.kt
@@ -0,0 +1,51 @@
+package advent_of_code.day7
+
+import java.io.File
+
+fun main() {
+    val tree = buildBagTreeTwo()
+    println(tree.getChildrenCountBFS(SHINY_GOLD))
+}
+
+val digits = "\\d+".toRegex()
+
+private fun Map<Color, Rule>.getChildrenCountBFS(color: Color): Int {
+    val children = getOrDefault(color, setOf())
+    if (children.isEmpty()) return 0
+    var total = 0
+    for(child in children) {
+        // findAll Returns a sequence of all occurrences of a regular expression (Sequence<MatchResult>)
+        // within the input string, beginning at the specified startIndex, which is 0 by default
+        // value is the substring from the input string captured by this match.
+        val count = digits.findAll(child).first().value.toInt()
+        // replace -> replaces all occurrences of this regular expression in the specified [input]
+        // string with specified [replacement] expression.
+        val bag = digits.replace(child, "").trim()
+        // we add count to the total and multiply count by the number of bags inside each bag
+        total += count + count * getChildrenCountBFS(bag)
+    }
+    return total
+}
+
+/**
+ * Builds a map of color of the container bag TO the set of specifications of included bags.
+ * The set of specifications is represented as the Set<String>.
+ */
+fun buildBagTreeTwo(): Map<Color, Rule> {
+    val rules = hashMapOf<Color, Rule>()
+    File("src/main/kotlin/advent_of_code/day7/input.txt")
+        .forEachLine { line ->
+            val (parent, allChildren) = line
+                .replace(Regex("bags?\\.?"), "")
+                .split("contain")
+                .map { it.trim() }
+            val rule: Set<String> =
+                if (allChildren.contains("no other")) {
+                    emptySet()
+                } else {
+                    allChildren.split(',').map { it.trim() }.toSet()
+                }
+            rules[parent] = rule
+        }
+    return rules
+}
