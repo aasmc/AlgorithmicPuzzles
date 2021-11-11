c6486bad6ebe0ee34ef478417a10b820bf34f0ab
aleksas <aasmc@yandex.ru>
2021-11-11 17:15:31 +0300

    Solve day10 Part 2 of advent of code 2020

--- src/main/kotlin/advent_of_code/day10/SolutionOne.kt
+++ src/main/kotlin/advent_of_code/day10/SolutionOne.kt
@@ -19,19 +19,34 @@ fun getAllAdaptersSorted(initialList: List<JoltageAdapter>): List<JoltageAdapter
 fun getDifferences(sortedAdapters: List<JoltageAdapter>): Map<Int, Int> {
     val result = hashMapOf<Int, Int>()
     var currentOutput = INITIAL_JOLTAGE
-    sortedAdapters.forEach { adapter ->
-        if (currentOutput in adapter.possibleInputRange()) {
-            result.compute(adapter.outputJoltage - currentOutput) { _, value ->
-                if (value == null) {
-                    1
-                } else {
-                    value + 1
+    sortedAdapters
+        .asSequence()
+        .forEach { adapter ->
+            if (currentOutput in adapter.possibleInputRange()) {
+                result.compute(adapter.outputJoltage - currentOutput) { _, value ->
+                    if (value == null) {
+                        1
+                    } else {
+                        value + 1
+                    }
                 }
+                currentOutput = adapter.outputJoltage
             }
-            currentOutput = adapter.outputJoltage
         }
-    }
     return result
+}
+
+fun getPermutations(sortedAdapters: List<JoltageAdapter>): Long {
+    val permutationsMap = hashMapOf(0 to 1L)
+    sortedAdapters
+        .forEach { adapter ->
+            // the number of possible ways to get to n-th adapter is the sum of all possible ways
+            // to get to n-th adapter from n-1, n-2 or n-3 adapters, if they are present of course
+            permutationsMap[adapter.outputJoltage] = (1..3).map { prevAdapter ->
+                permutationsMap.getOrDefault(adapter.outputJoltage - prevAdapter, 0)
+            }.sum()
+        }
+    return permutationsMap.getValue(sortedAdapters.last().outputJoltage)
 }
 
 fun processResult(result: Map<Int, Int>) {
@@ -49,6 +64,8 @@ fun main() {
     val sorted = getAllAdaptersSorted(input)
     val diff = getDifferences(sorted)
     processResult(diff)
+    val permutations = getPermutations(sorted)
+    println("Permutations: $permutations")
 }
 
 
