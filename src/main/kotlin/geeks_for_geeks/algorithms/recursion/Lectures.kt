62cbcdb8c85e976ae13314b600ea2bf0f36d3903
aleksas <aasmc@yandex.ru>
2021-11-26 16:26:41 +0300

    Add logBaseTwoRecursive function

--- src/main/kotlin/geeks_for_geeks/algorithms/recursion/Lectures.kt
+++ src/main/kotlin/geeks_for_geeks/algorithms/recursion/Lectures.kt
@@ -1,2 +1,8 @@
 package geeks_for_geeks.algorithms.recursion
 
+fun logBaseTwoRecursive(num: Int): Int   {
+    if(num == 1) {
+        return 0
+    }
+    return 1 + logBaseTwoRecursive(num / 2)
+}
