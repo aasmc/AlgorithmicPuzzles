df71646a4cd82bf5bc1682399d271a8a897410fa
aleksas <aasmc@yandex.ru>
2022-01-25 17:27:50 +0300

    Solve advent of code 2021 day 02

--- src/main/kotlin/advent_of_code/year_2021/day_02/Solution.kt
+++ src/main/kotlin/advent_of_code/year_2021/day_02/Solution.kt
@@ -1,5 +1,61 @@
 package advent_of_code.year_2021.day_02
 
-fun main() {
+import advent_of_code.readLinesFromFile
 
+fun main() {
+    val commands = readLinesFromFile("year_2021/day_02")
+        .map { line ->
+            val (dir, dst) = line.split(" ")
+            val direction = when (dir) {
+                "forward" -> Direction.FORWARD
+                "up" -> Direction.UP
+                "down" -> Direction.DOWN
+                else -> throw IllegalStateException("Unknown Direction $dir")
+            }
+            Command(direction, dst.toInt())
+        }
+
+    partOne(commands)
+    partTwo(commands)
+}
+
+fun partTwo(commands: List<Command>) {
+    var aim = 0
+    var horizontalPosition = 0
+    var depth = 0
+    commands.forEach { command ->
+        when(command.direction) {
+            Direction.FORWARD -> {
+                horizontalPosition += command.distance
+                depth += aim * command.distance
+            }
+            Direction.UP -> aim -= command.distance
+            Direction.DOWN -> aim += command.distance
+        }
+    }
+    println(depth * horizontalPosition)
+}
+
+data class Command(
+    val direction: Direction,
+    val distance: Int
+)
+
+enum class Direction {
+    FORWARD,
+    UP,
+    DOWN
+}
+
+fun partOne(commands: List<Command>) {
+    var horizontalPosition = 0
+    var depth = 0
+    commands.forEach { command ->
+        when(command.direction) {
+            Direction.FORWARD -> horizontalPosition += command.distance
+            Direction.UP -> depth -= command.distance
+            Direction.DOWN -> depth += command.distance
+        }
+    }
+    println(horizontalPosition * depth)
 }
