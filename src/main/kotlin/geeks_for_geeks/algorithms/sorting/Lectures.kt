e1a13c91b08ae6cc6aafdf6ea0ad7615eb00f95d
aleksas <aasmc@yandex.ru>
2022-01-31 16:25:12 +0300

    Add merge sort implementation in GFG sorting track

--- src/main/kotlin/geeks_for_geeks/algorithms/sorting/Lectures.kt
+++ src/main/kotlin/geeks_for_geeks/algorithms/sorting/Lectures.kt
@@ -63,8 +63,74 @@ fun insertionSort(arr: IntArray) {
 }
 
 
+fun mergeSortedArrays(first: IntArray, second: IntArray): IntArray {
+    val result = IntArray(first.size + second.size)
+    var firstIdx = 0
+    var secondIdx = 0
+    var currentIndex = 0
+    while (firstIdx < first.size && secondIdx < second.size) {
+        if (first[firstIdx] <= second[secondIdx]) {
+            result[currentIndex++] = first[firstIdx++]
+        } else {
+            result[currentIndex++] = second[secondIdx++]
+        }
+    }
+    while (firstIdx < first.size) {
+        result[currentIndex++] = first[firstIdx++]
+    }
+
+    while (secondIdx < second.size) {
+        result[currentIndex++] = second[secondIdx++]
+    }
+    return result
+}
 
 
+fun merge(arr: IntArray, low: Int, mid: Int, high: Int) {
+    val left = IntArray(mid - low + 1) { index ->
+        arr[index + low]
+    }
+    val right = IntArray(high - mid) { index ->
+        arr[index + mid + 1]
+    }
+    var firstIdx = 0
+    var secondIdx = 0
+    var currentIdx = low
+    while (firstIdx < left.size && secondIdx < right.size) {
+        if (left[firstIdx] <= right[secondIdx]) {
+            arr[currentIdx++] = left[firstIdx++]
+        } else {
+            arr[currentIdx++] = right[secondIdx++]
+        }
+    }
+
+    while (firstIdx < left.size) {
+        arr[currentIdx++] = left[firstIdx++]
+    }
+
+    while (secondIdx < right.size) {
+        arr[currentIdx++] = right[secondIdx++]
+    }
+}
+
+/**
+ * Sorts a given array of integers.
+ * Stable sort.
+ * Time complexity: O(n^2)
+ * Space complexity O(n)
+ */
+fun mergeSort(arr: IntArray) {
+    mergeSortHelper(arr, 0, arr.lastIndex)
+}
+
+private fun mergeSortHelper(arr: IntArray, low: Int, high: Int) {
+    if (high > low) {
+        val mid = low + (high - low) / 2
+        mergeSortHelper(arr, low, mid)
+        mergeSortHelper(arr, mid + 1, high)
+        merge(arr, low, mid, high)
+    }
+}
 
 
 
--- src/test/kotlin/geeks_for_geeks/algorithms/sorting/GFGSortingTest.kt
+++ src/test/kotlin/geeks_for_geeks/algorithms/sorting/GFGSortingTest.kt
@@ -7,7 +7,7 @@ internal class GFGSortingTest {
 
     @Test
     fun bubbleSort_correct() {
-        val arr = intArrayOf(5,2,7,1,3,9,10)
+        val arr = intArrayOf(5, 2, 7, 1, 3, 9, 10)
         bubbleSort(arr)
         for (i in 0 until arr.lastIndex - 1) {
             assertTrue(arr[i] <= arr[i + 1])
@@ -16,7 +16,7 @@ internal class GFGSortingTest {
 
     @Test
     fun selectionSort_correct() {
-        val arr = intArrayOf(5,2,7,1,3,9,10)
+        val arr = intArrayOf(5, 2, 7, 1, 3, 9, 10)
         selectionSort(arr)
         for (i in 0 until arr.lastIndex - 1) {
             assertTrue(arr[i] <= arr[i + 1])
@@ -25,8 +25,37 @@ internal class GFGSortingTest {
 
     @Test
     fun insertionSort_correct() {
-        val arr = intArrayOf(5,2,7,1,3,9,10)
+        val arr = intArrayOf(5, 2, 7, 1, 3, 9, 10)
         insertionSort(arr)
+        for (i in 0 until arr.lastIndex - 1) {
+            assertTrue(arr[i] <= arr[i + 1])
+        }
+    }
+
+    @Test
+    fun mergeSortedArrays_correct() {
+        val first = intArrayOf(1, 3, 5, 7, 9)
+        val second = intArrayOf(2, 4, 6, 8, 10)
+        val result = mergeSortedArrays(first, second)
+        assert(result.size == 10)
+        for (i in 0 until result.lastIndex) {
+            assertTrue(result[i] <= result[i + 1])
+        }
+    }
+
+    @Test
+    fun merge_correct() {
+        val first = intArrayOf(1, 3, 5, 7, 9, 2, 4, 6, 8, 10)
+        merge(first, 0, 4, 9)
+        for (i in 0 until first.lastIndex) {
+            assertTrue(first[i] <= first[i + 1])
+        }
+    }
+
+    @Test
+    fun mergeSort_correct() {
+        val arr = intArrayOf(5, 2, 7, 1, 3, 9, 10)
+        mergeSort(arr)
         for (i in 0 until arr.lastIndex - 1) {
             assertTrue(arr[i] <= arr[i + 1])
         }
