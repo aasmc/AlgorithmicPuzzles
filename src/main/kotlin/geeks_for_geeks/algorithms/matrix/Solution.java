fd7036bd4e1bf25dcf104878f661bfed4360debe
aleksas <aasmc@yandex.ru>
2022-05-20 17:54:20 +0300

    Solve determinant of matrix in GFG matrix track

--- src/main/kotlin/geeks_for_geeks/algorithms/matrix/Solution.java
+++ src/main/kotlin/geeks_for_geeks/algorithms/matrix/Solution.java
@@ -99,4 +99,121 @@ public class Solution {
         }
         return result;
     }
+
+    /**
+     * Write a program to find the transpose of a square matrix of size N*N.
+     * Transpose of a matrix is obtained by changing rows to columns and columns to rows.
+     */
+    static void transpose(int matrix[][], int n)
+    {
+        // code here
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][], int n)
+    {
+        // code here
+        transpose(matrix, n);
+        for (int i = 0; i < n / 2; i++) {
+            int[] tmp = matrix[n - i - 1];
+            matrix[n - i - 1] = matrix[i];
+            matrix[i] = tmp;
+        }
+    }
+
+    /**
+     * Given a square matrix of size N x N. The task is to find the determinant of this matrix.
+     *
+     * Example 1:
+     *      Input:
+     *      N = 4
+     *      matrix[][] = {{1, 0, 2, -1},
+     *                    {3, 0, 0, 5},
+     *                    {2, 1, 4, -3},
+     *                    {1, 0, 5, 0}}
+     *      Output: 30
+     *      Explanation:
+     *      Determinant of the given matrix is 30.
+     *
+     * Example 2:
+     *      Input:
+     *      N = 3
+     *      matrix[][] = {{1, 2, 3},
+     *                    {4, 5, 6},
+     *                    {7, 10, 9}}
+     *      Output: 12
+     *      Explanation:
+     *      Determinant of the given matrix is 12.
+     */
+    static int determinantOfMatrix(int matrix[][], int n)
+    {
+        if (n == 1) {
+            return matrix[0][0];
+        }
+        else if (n == 2) {
+            return calculateDeterminantForTwoByTwoMatrix(matrix);
+        } else if (n == 3){
+            return calculateDeterminantForThreeByThreeMatrix(matrix);
+        } else {
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                int[][] tmpMatrix = createTmpMatrix(matrix, i);
+                int num = matrix[0][i];
+                if (i % 2 == 0) {
+                    res += (num * determinantOfMatrix(tmpMatrix, n - 1));
+                } else {
+                    res -= (num * determinantOfMatrix(tmpMatrix, n - 1));
+                }
+            }
+            return res;
+        }
+    }
+
+    private static int calculateDeterminantForTwoByTwoMatrix(int[][] matrix) {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+
+    private static int calculateDeterminantForThreeByThreeMatrix(int[][] matrix) {
+        int[][] tmpMatrix = new int[2][2];
+        tmpMatrix[0][0] = matrix[1][1];
+        tmpMatrix[0][1] = matrix[1][2];
+        tmpMatrix[1][0] = matrix[2][1];
+        tmpMatrix[1][1] = matrix[2][2];
+        int aDeterminant = calculateDeterminantForTwoByTwoMatrix(tmpMatrix);
+        aDeterminant *= matrix[0][0];
+
+        tmpMatrix[0][0] = matrix[1][0];
+        tmpMatrix[1][0] = matrix[2][0];
+
+        int bDeterminant = calculateDeterminantForTwoByTwoMatrix(tmpMatrix);
+        bDeterminant *= matrix[0][1];
+
+        tmpMatrix[0][1] = matrix[1][1];
+        tmpMatrix[1][1] = matrix[2][1];
+
+        int cDeterminant = calculateDeterminantForTwoByTwoMatrix(tmpMatrix);
+        cDeterminant *= matrix[0][2];
+        return aDeterminant - bDeterminant + cDeterminant;
+    }
+
+    private static int[][] createTmpMatrix(int[][] from, int excludeCol) {
+        int[][] result = new int[from.length - 1][from.length - 1];
+        for (int i = 1; i < from.length; i++) {
+            int colIdx = 0;
+            for (int j = 0; j < from.length; j++) {
+                if (j != excludeCol) {
+                    result[i - 1][colIdx++] = from[i][j];
+                }
+            }
+        }
+        return result;
+    }
+
 }
