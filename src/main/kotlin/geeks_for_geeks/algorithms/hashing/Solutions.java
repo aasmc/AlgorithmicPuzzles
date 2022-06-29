47b1b3b3eb64fc48f66bfa1339b7778e74b18945
Alex Alexeyev <aasmc@yandex.ru>
2022-06-29 22:12:32 +0300

    Solve quadratic probing in GFG hashing track

--- src/main/kotlin/geeks_for_geeks/algorithms/hashing/Solutions.java
+++ src/main/kotlin/geeks_for_geeks/algorithms/hashing/Solutions.java
@@ -150,6 +150,60 @@ public class Solutions {
     }
 
 
+    /**
+     * Quadratic probing is a collision handling technique in hashing. Quadratic probing
+     * says that whenever a collision occurs, search for i^2 position.
+     *
+     * Given an array of integers and a Hash table. Fill the elements of the array into
+     * the hash table by using Quadratic Probing in case of collisions.
+     *
+     * Example 1:
+     *      Input:
+     *      hashSize = 11
+     *      N = 4
+     *      Array[] = {21,10,32,43}
+     *      Output:
+     *      10 -1 -1 32 -1 -1 -1 -1 43 -1 21
+     *      Explanation: 21%11=10 so 21 goes into
+     *      hashTable[10] position. 10%11=10.
+     *      hashTable[10] is already filled so we try
+     *      for (10+1^2)%11=0 position. hashTable[0]
+     *      is empty so we put 10 there. 32%11=10.
+     *      hashTable[10] is filled. We try
+     *      (32+1^2)%11=0. But hashTable[0] is also
+     *      already filled. We try (32+2^2)%11=3.
+     *      hashTable[3] is empty so we put 32 in
+     *      hashTable[3] position. 43 uses
+     *      (43+32)%11=8. We put it in hashTable[8].
+     * Example 2:
+     *      Input:
+     *      hashSize = 11
+     *      N = 4
+     *      Array[] = {880,995,647,172 }
+     *      Output:
+     *      880 -1 -1 -1 -1 995 -1 172 -1 647 -1
+     *      Explanation: Using the similar approach
+     *      as used in above explanation we will get
+     *      the output like
+     *      880 -1 -1 -1 -1 995 -1 172 -1 647 -1.
+     */
+    static void quadraticProbing(int[] hash, int hash_size, int[] arr, int N)
+    {
+        //Your code here
+        Arrays.fill(hash, -1);
+        for (int elem : arr) {
+            int index = elem % hash_size;
+            if (hash[index] != -1 && hash[index] != elem) {
+                int k = 1;
+                index = (int) ((elem + Math.pow(k, 2)) % hash_size);
+                while (hash[index] != -1 && hash[index] != elem) {
+                    ++k;
+                    index = (int) ((elem + Math.pow(k, 2)) % hash_size);
+                }
+            }
+            hash[index] = elem;
+        }
+    }
 
 }
 
