1c6a6c864d4eba6999027d17da8bd566f222d782
Alex Alexeyev <aasmc@yandex.ru>
2022-06-29 22:45:16 +0300

    Solve first repeated in GFG hashing track

--- src/main/kotlin/geeks_for_geeks/algorithms/hashing/Solutions.java
+++ src/main/kotlin/geeks_for_geeks/algorithms/hashing/Solutions.java
@@ -3,6 +3,7 @@ package geeks_for_geeks.algorithms.hashing;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Solutions {
 
@@ -251,6 +252,49 @@ public class Solutions {
         return hash.values().stream().filter(num -> num == 1).count();
     }
 
+    static ArrayList<Integer> printNonRepeated(int[] arr, int n)
+    {
+        // add your code here
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int elem : arr) {
+            hash.merge(elem, 1, Integer::sum);
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int elem : arr) {
+            if (hash.get(elem) == 1) {
+                result.add(elem);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Given an array arr[] of size n, find the first repeating element. The element should occurs more
+     * than once and the index of its first occurrence should be the smallest.
+     *
+     * Input:
+     * n = 7
+     * arr[] = {1, 5, 3, 4, 3, 5, 6}
+     * Output: 2
+     * Explanation:
+     * 5 is appearing twice and
+     * its first appearence is at index 2
+     * which is less than 3 whose first
+     * occuring index is 3.
+     */
+    public static int firstRepeated(int[] arr, int n) {
+        // Your code here
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int elem : arr) {
+            hash.merge(elem, 1, Integer::sum);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (hash.get(arr[i]) > 1) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
 }
 
 
