be01be9ade93f8277aa0db34223d3f6dcd4a00e4
Alex Alexeyev <aasmc@yandex.ru>
2022-07-30 17:57:47 +0300

    Solve count subarrays with zero sum in GFG hashing track

--- src/main/kotlin/geeks_for_geeks/algorithms/hashing/Solutions.java
+++ src/main/kotlin/geeks_for_geeks/algorithms/hashing/Solutions.java
@@ -622,6 +622,46 @@ public class Solutions {
         }
         return result;
     }
+
+    /**
+     * You are given an array arr[] of size n.
+     * Find the total count of sub-arrays having their sum equal to 0.
+     * Example 1:
+     *      Input:
+     *      n = 6
+     *      arr[] = {0,0,5,5,0,0}
+     *      Output: 6
+     *      Explanation: The 6 subarrays are
+     *      [0], [0], [0], [0], [0,0], and [0,0].
+     *
+     * Example 2:
+     *      Input:
+     *      n = 10
+     *      arr[] = {6,-1,-3,4,-2,2,4,6,-12,-7}
+     *      Output: 4
+     *      Explanation: The 4 subarrays are [-1 -3 4]
+     *      [-2 2], [2 4 6 -12], and [-1 -3 4 -2 2]
+     *
+     * Expected Time Complexity : O(n)
+     * Expected Auxiliary Space : O(n)
+     */
+    public static long findSubarray(long[] arr ,int n)
+    {
+        long count = 0;
+        Map<Integer, Integer> prefixSumToCount = new HashMap<>();
+        int prefixSum = 0;
+        for (int i = 0; i < n; i++) {
+            prefixSum += arr[i];
+            if (prefixSum == 0) {
+                prefixSumToCount.merge(0, 1, Integer::sum);
+                count += prefixSumToCount.getOrDefault(prefixSum, 0);
+            } else {
+                count += prefixSumToCount.getOrDefault(prefixSum, 0);
+                prefixSumToCount.merge(prefixSum, 1, Integer::sum);
+            }
+        }
+        return count;
+    }
 }
 
 
