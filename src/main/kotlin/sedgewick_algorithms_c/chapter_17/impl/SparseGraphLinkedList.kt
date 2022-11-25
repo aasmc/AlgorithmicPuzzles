180e577f315c6d1956328f530d2995d043f2b5fb
Alex Alexeyev <aasmc@yandex.ru>
2022-11-25 22:25:06 +0300

    Add implementation of Sparse Graph represented as an adjacency list of doubly linked nodes.

--- src/main/kotlin/sedgewick_algorithms_c/chapter_17/impl/SparseGraphLinkedList.kt
+++ src/main/kotlin/sedgewick_algorithms_c/chapter_17/impl/SparseGraphLinkedList.kt
@@ -4,20 +4,28 @@ import sedgewick_algorithms_c.chapter_17.adt.AdjIterator
 import sedgewick_algorithms_c.chapter_17.adt.Edge
 import sedgewick_algorithms_c.chapter_17.adt.Graph
 import java.util.*
-import kotlin.collections.ArrayList
 
 class SparseGraphLinkedList(
     private val vertexCount: Int,
     private val diGraph: Boolean
 ) : Graph {
 
+    private data class Node(
+        val from: Int,
+        val to: Int,
+        var prev: Node? = null,
+        var next: Node? = null
+    )
+
+    private val edgeStorage: MutableMap<Int, Node> = hashMapOf()
+
     private var edgeCount = 0
 
-    private val adjStorage = ArrayList<LinkedList<Int>>(vertexCount)
+    private val adjStorage = ArrayList<Node?>(vertexCount)
 
     init {
         for (i in 0 until vertexCount) {
-            adjStorage.add(LinkedList())
+            adjStorage.add(null)
         }
     }
 
@@ -33,31 +41,88 @@ class SparseGraphLinkedList(
         return edgeCount
     }
 
+    /**
+     * Inserts an edge to the graph in constant time.
+     * Prevents inserting an edge, that is already in the graph.
+     */
     override fun insert(edge: Edge) {
+        if (checkEdgeExists(edge)) return
         val (v, w) = edge
-        // insert w as the head of the singly linked list
-        adjStorage[v].addLast(w)
-        if (!diGraph) adjStorage[w].addLast(v)
+        addNodeToAdjList(v, w)
+        if (!diGraph) addNodeToAdjList(w, v)
         ++edgeCount
     }
 
+    private fun addNodeToAdjList(sourceVertex: Int, toInsert: Int) {
+        val prev = adjStorage[sourceVertex]
+
+        val node = Node(
+            from = sourceVertex,
+            to = toInsert,
+            prev = prev,
+            next = null
+        )
+        adjStorage[sourceVertex] = node
+        edgeStorage[computeEdgeKey(sourceVertex, toInsert)] = node
+    }
+
+    /**
+     * Removes the given edge from the graph in constant time.
+     * If there's no edge in the graph, then nothing happens.
+     */
     override fun remove(edge: Edge) {
         val (v, w) = edge
-        adjStorage[v].remove(w)
+        val fromKey = computeEdgeKey(v, w)
+        val toRemoveFrom = edgeStorage[fromKey]
+            ?: return
+
+        unlink(toRemoveFrom, v)
+        edgeStorage.remove(fromKey)
         --edgeCount
         if (!diGraph) {
-            adjStorage[w].remove(v)
+            val toKey = computeEdgeKey(w, v)
+            val toRemoveTo = edgeStorage[toKey]
+                ?: throw IllegalStateException(
+                    "Undirected graph is in inconsistent state: vertices $v and $w have " +
+                            "not been stored in each other's adjacency lists!"
+                )
+            unlink(toRemoveTo, w)
+            edgeStorage.remove(toKey)
+        }
+    }
+
+    private fun unlink(toRemoveFrom: Node, v: Int) {
+        val prev = toRemoveFrom.prev
+        val next = toRemoveFrom.next
+        if (prev == null && next == null) {
+            adjStorage[v] = null
+        } else if (prev == null) {
+            next!!.prev = null
+            toRemoveFrom.next = null
+            adjStorage[v] = next
+        } else if (next == null) {
+            adjStorage[v] = prev
+            prev.next = null
+            toRemoveFrom.prev = null
+        } else {
+            prev.next = next
+            next.prev = prev
+            toRemoveFrom.prev = null
+            toRemoveFrom.next = null
         }
     }
 
 
+    /**
+     * Checks if the given edge exists in the graph in costant time.
+     */
     override fun checkEdgeExists(edge: Edge): Boolean {
         val (v, w) = edge
-        val vList = adjStorage[v]
-        val existsInV = checkVertexInList(vList, w)
+        val fromKey = computeEdgeKey(v, w)
+        val existsInV = edgeStorage[fromKey] != null
         if (!diGraph) {
-            val wList = adjStorage[w]
-            val existsInW = checkVertexInList(wList, v)
+            val toKey = computeEdgeKey(w, v)
+            val existsInW = edgeStorage[toKey] != null
             if ((!existsInV && existsInW) || (existsInV && !existsInW)) {
                 throw IllegalStateException(
                     "Undirected graph is in inconsistent state! Vertex $v and vertex " +
@@ -77,21 +142,28 @@ class SparseGraphLinkedList(
         return AdjListAdjIterator(v)
     }
 
+    private fun computeEdgeKey(from: Int, to: Int): Int {
+        return from * vertexCount + to
+    }
+
     private inner class AdjListAdjIterator(
         val vertex: Int
     ) : AdjIterator {
-        private var curListIterator = adjStorage[vertex].iterator()
+        private var next: Node? = null
         override fun beg(): Int {
-            curListIterator = adjStorage[vertex].iterator()
-            return nxt()
+            next = adjStorage[vertex]
+            return if (next != null) next!!.to else -1
         }
 
         override fun nxt(): Int {
-            return curListIterator.next()
+            if (next != null) {
+                next = next!!.next
+            }
+            return if (next != null) next!!.to else -1
         }
 
         override fun end(): Boolean {
-            return curListIterator.hasNext()
+            return next == null
         }
     }
 }
--- src/test/kotlin/sedgewick_algorithms_c/chapter_17/util/GraphCreator.kt
+++ src/test/kotlin/sedgewick_algorithms_c/chapter_17/util/GraphCreator.kt
@@ -3,6 +3,7 @@ package sedgewick_algorithms_c.chapter_17.util
 import sedgewick_algorithms_c.chapter_17.adt.Edge
 import sedgewick_algorithms_c.chapter_17.adt.Graph
 import sedgewick_algorithms_c.chapter_17.impl.DenseGraph
+import sedgewick_algorithms_c.chapter_17.impl.SparseGraphLinkedList
 import sedgewick_algorithms_c.chapter_17.impl.SparseMultiGraph
 
 object GraphCreator {
@@ -32,7 +33,24 @@ object GraphCreator {
         }
     }
 
+    fun createUndirectedSparseGraphLinkedList(): Graph {
+        return SparseMultiGraph(7, false).apply {
+            insert(Edge(0, 1))
+            insert(Edge(0, 2))
+            insert(Edge(0, 3))
+            insert(Edge(1, 3))
+            insert(Edge(1, 4))
+            insert(Edge(2, 4))
+            insert(Edge(3, 4))
+            insert(Edge(5, 6))
+        }
+    }
+
     fun createUndirectedEmptySparseGraph(): Graph {
         return SparseMultiGraph(7, false)
+    }
+
+    fun createUndirectedEmptyGraphLinkedList(): Graph {
+        return SparseGraphLinkedList(7, false)
     }
 }
