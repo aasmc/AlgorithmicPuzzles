ac3b511132cdef73a8bc71c721b27f64ca1d0af2
Alex Alexeyev <aasmc@yandex.ru>
2022-12-10 13:54:49 +0300

    Solve advent of code 2022 day 10 part two

--- src/main/kotlin/advent_of_code/year_2022/day_10/Solution.kt
+++ src/main/kotlin/advent_of_code/year_2022/day_10/Solution.kt
@@ -17,6 +17,82 @@ fun main() {
     cpu.executeAllInstructions()
     val sum = cpu.getSumOfSignalStrengths()
     println(sum)
+    val crt = CRT(lines)
+    crt.executeAllInstructions()
+    crt.drawScreen()
+}
+
+data class Sprite(
+    val pixels: IntRange
+)
+
+class CRT(
+    private val ops: List<Operation>
+) {
+    private val screen: MutableList<CharArray> = MutableList(6) {
+        CharArray(40) { '.' }
+    }
+    private var currentSprite: Sprite = Sprite(0..2)
+    private var rowPosition = 0
+    private var cycleCount: Int by Delegates.observable(0) { _, _, new ->
+        if (new % 40 == 0) {
+            rowPosition++
+        }
+        if (new == 240){
+            cycleComplete = true
+        }
+        if (checkColIntersectsWithSprite()) {
+            screen[rowPosition][colPosition] = '#'
+        }
+        incrementAndGetColPosition()
+    }
+    private var cycleComplete = false
+    private var registerState: Int by Delegates.observable(1) { _, _, new ->
+        currentSprite = Sprite((new - 1)..(new + 1))
+    }
+    private var instructionPointer = 0
+    private var colPosition = 0
+
+    private fun incrementAndGetColPosition(): Int {
+        colPosition++
+        if (colPosition % 40 == 0) {
+            colPosition = 0
+        }
+        return colPosition
+    }
+
+    private fun checkColIntersectsWithSprite(): Boolean {
+        return colPosition in currentSprite.pixels
+    }
+
+    fun executeInstruction() {
+        val op = ops[instructionPointer++]
+        when(op) {
+            is Operation.Add -> {
+                ++cycleCount
+                ++cycleCount
+                registerState += op.value
+            }
+            is Operation.Noop -> {
+                ++cycleCount
+            }
+        }
+    }
+
+    fun executeAllInstructions() {
+        while (!cycleComplete) {
+            executeInstruction()
+        }
+    }
+
+    fun drawScreen() {
+        for (line in screen) {
+            for (ch in line) {
+                print(ch)
+            }
+            println()
+        }
+    }
 }
 
 class CPU(
