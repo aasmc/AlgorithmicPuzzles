b024e013a617812e9cd737935cd40482d081ba5a
Alex Alexeyev <aasmc@yandex.ru>
2023-02-22 00:15:03 +0300

    solve task 3 yandex algo training (java)

--- src/main/kotlin/yandex_algo_training/contest01/diego03/Solution.java
+++ src/main/kotlin/yandex_algo_training/contest01/diego03/Solution.java
@@ -3,13 +3,78 @@ package yandex_algo_training.contest01.diego03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+        solve();
+    }
+
+
+    private static void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int diegoNum = Integer.parseInt(br.readLine());
+        String diegoStr = br.readLine().trim();
+
+        int[] diego = readStickers(diegoStr, diegoNum);
+        int numCollections = Integer.parseInt(br.readLine());
+        String collStr = br.readLine();
+        int[] colls = readStickers(collStr, numCollections);
+
+        int[] sortedDiego = removeDuplicatesAndSort(diego);
+        int[] res = countStickers(colls, sortedDiego);
+        Arrays.stream(res).forEach(System.out::println);
+        br.close();
+    }
+
+    private static int[] removeDuplicatesAndSort(int[] arr) {
+        if (arr.length == 1) return arr;
+        Arrays.sort(arr);
+        int positionAfterUnique = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[positionAfterUnique - 1]) {
+                arr[positionAfterUnique++] = arr[i];
+            }
+        }
+        int[] res = new int[positionAfterUnique];
+        System.arraycopy(arr, 0, res, 0, positionAfterUnique);
+        return res;
+    }
+    private static int[] readStickers(String str, int num) {
+        if (num > 0) {
+            int[] res = new int[num];
+            int idx = 0;
+            for (String n: str.split(" ")) {
+                res[idx++] = Integer.parseInt(n);
+            }
+            return res;
+        }
+        return new int[0];
+    }
+
+    private static int[] countStickers(int[] colls, int[] diego) {
+        if (colls.length == 0) {
+            return new int[0];
+        }
+        int[] res = new int[colls.length];
+        if (diego.length == 0) {
+            return res;
+        }
+        for (int i = 0; i < colls.length; i++) {
+            int target = colls[i];
+            int idx = Arrays.binarySearch(diego, target);
+            if (idx < 0) {
+                // (-(insertion point) - 1)
+                res[i] = Math.abs(idx + 1);
+            } else {
+                res[i] = idx;
+            }
+        }
+        return res;
+    }
+
+
+    private static void solveV1() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int diegoNum = Integer.parseInt(br.readLine());
         String diegoStr = br.readLine().trim();
@@ -36,7 +101,7 @@ public class Solution {
 
     private static List<Integer> countStickers(List<Integer> colls, TreeSet<Integer> diego) {
         if (colls.isEmpty()) return Collections.emptyList();
-        List<Integer> res = new ArrayList<>();
+        List<Integer> res = new ArrayList<>(colls.size());
         if (diego.isEmpty()) {
             for (int i = 0; i < colls.size(); ++i) {
                 res.add(0);
