59976af030a00e1a159ce120f76d09ca807fbcf3
Alex Alexeyev <aasmc@yandex.ru>
2023-02-28 12:01:29 +0300

    solve task 30 of dynamic programming

--- src/main/kotlin/yandex_algo_training/contest05/fifth/solution.kt
+++ src/main/kotlin/yandex_algo_training/contest05/fifth/solution.kt
@@ -1,5 +1,40 @@
 package yandex_algo_training.contest05.fifth
 
 fun main() {
+    val n = readLine()!!.trim().toInt()
+    val first = readLine()!!.trim().split(" ").map { it.toInt() }
+    val m = readLine()!!.trim().toInt()
+    val second = readLine()!!.trim().split(" ").map { it.toInt() }
+    val width = n + 1
+    val height = m + 1
+    val dp = Array<IntArray>(width) {
+        IntArray(height) { 0 }
+    }
+    for (i in 1 until width) {
+        for (j in 1 until height) {
+            if (first[i - 1] == second[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = maxOf(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
 
+    val res = mutableListOf<Int>()
+    var i = first.size
+    var j = second.size
+    while (i != 0 && j != 0) {
+        if (first[i - 1] == second[j - 1]) {
+            res.add(first[i - 1])
+            --i
+            --j
+        } else if (dp[i][j - 1] > dp[i - 1][j]) {
+            --j
+        } else {
+            --i
+        }
+    }
+    res.reverse()
+    val answer = res.joinToString(separator = " ")
+    println(answer)
 }
--- src/main/kotlin/yandex_algo_training/contest05/fourth/solution.kt
+++ src/main/kotlin/yandex_algo_training/contest05/fourth/solution.kt
@@ -1,5 +1,25 @@
 package yandex_algo_training.contest05.fourth
 
 fun main() {
-
+    val n = readLine()!!.toInt()
+    val days = IntArray(n)
+    repeat(n) { idx ->
+        days[idx] = readLine()!!.toInt()
+    }
+    val dp = Array<IntArray>(n + 2) {
+        IntArray(n + 2) { 0 }
+    }
+    for (i in 1..n) {
+        dp[i][0] = days[i - 1]
+    }
+    for (i in 1..n) {
+        dp[0][i] = i
+    }
+    for (j in 1 until dp.size - 1) {
+        for (i in 1 until dp[0].size - 1) {
+            val min = minOf(dp[i - 1][j + 1], dp[i - 1][j - 1], dp[i - 1][j])
+            dp[i][j] = min
+        }
+    }
+    println()
 }
