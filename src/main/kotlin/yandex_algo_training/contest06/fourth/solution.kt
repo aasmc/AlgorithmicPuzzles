16b0dd9fbf25ba344b39fa4ba5e15cfd3f24f31a
Alex Alexeyev <aasmc@yandex.ru>
2023-03-02 17:44:34 +0300

    add some comments to graph tasks

--- src/main/kotlin/yandex_algo_training/contest06/fourth/solution.kt
+++ src/main/kotlin/yandex_algo_training/contest06/fourth/solution.kt
@@ -22,10 +22,11 @@ fun main() {
 }
 
 fun topoSort(g: Array<ArrayList<Int>>, vertexCount: Int): List<Int> {
-    if (detectCycle(g, vertexCount)) return emptyList()
+    if (detectCycleInDirectedGraph(g, vertexCount)) return emptyList()
 
     val stack = ArrayDeque<Int>()
     val visited = BooleanArray(vertexCount + 1)
+
     fun dfs(source: Int) {
         visited[source] = true
         for (v in g[source]) {
@@ -33,6 +34,7 @@ fun topoSort(g: Array<ArrayList<Int>>, vertexCount: Int): List<Int> {
                 dfs(v)
             }
         }
+        // place the farthest vertex reachable from initial source on the stack
         stack.push(source)
     }
 
@@ -48,8 +50,9 @@ fun topoSort(g: Array<ArrayList<Int>>, vertexCount: Int): List<Int> {
     return result
 }
 
-fun detectCycle(g: Array<ArrayList<Int>>, vertexCount: Int): Boolean {
+fun detectCycleInDirectedGraph(g: Array<ArrayList<Int>>, vertexCount: Int): Boolean {
     val visited = BooleanArray(vertexCount + 1)
+    // tracks whether current vertex takes part in recursive call
     val recursionStack = BooleanArray(vertexCount + 1)
     fun dfs(source: Int): Boolean {
         visited[source] = true
@@ -57,6 +60,8 @@ fun detectCycle(g: Array<ArrayList<Int>>, vertexCount: Int): Boolean {
         for (v in g[source]) {
             if (!visited[v] && dfs(v)) {
                 return true
+                // if this vertex was visited and was on recursion stask
+                // we have a cycle
             } else if (recursionStack[v]) {
                 return true
             }
---
+++ src/main/kotlin/yandex_algo_training/contest06/third/solution.kt
@@ -0,0 +1,46 @@
+package yandex_algo_training.contest06.third
+
+import java.util.ArrayList
+
+fun main() {
+    val (n, m) = readLine()!!.trim().split(" ").map { it.toInt() }
+    val g = Array<ArrayList<Int>>(n + 1) {
+        ArrayList()
+    }
+    repeat(m) {
+        val (from, to)  = readLine()!!.trim().split(" ").map { it.toInt() }
+        g[from].add(to)
+        g[to].add(from)
+    }
+    if (isBipartite(g, n)) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
+
+fun isBipartite(g: Array<ArrayList<Int>>, vertexCount: Int): Boolean {
+    val colors = arrayOfNulls<Boolean>(vertexCount + 1)
+    fun dfs(v: Int, color: Boolean): Boolean {
+        // our neighbour tried to color us with the same color
+        // graph is not bipartite
+        colors[v]?.let {
+            return@dfs it == color
+        }
+        // color this vertex
+        colors[v] = color
+        for (u in g[v]) {
+            // try to color all our adjacent vertices with different color
+            // if failed - graph is not bipartite
+            if (!dfs(u, !color)) return false
+        }
+        // if success -> graph is bipartite (connected component of a graph)
+        return true
+    }
+    for (i in 1..vertexCount) {
+        if (colors[i] == null) {
+            if (!dfs(i, true)) return false
+        }
+    }
+    return true
+}
