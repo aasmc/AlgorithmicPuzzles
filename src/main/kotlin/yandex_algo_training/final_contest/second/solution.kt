08c1428a26b5b4be1efad5603989f01133b378d2
Alex Alexeyev <aasmc@yandex.ru>
2023-03-13 22:17:56 +0300

    add description of possible solutions

--- src/main/kotlin/yandex_algo_training/final_contest/second/solution.kt
+++ src/main/kotlin/yandex_algo_training/final_contest/second/solution.kt
@@ -1,9 +1,20 @@
 package yandex_algo_training.final_contest.second
 
+import java.util.PriorityQueue
+
 fun main() {
     val (n, w) = readLine()!!.trim().split(" ").map { it.toInt() }
-    val employees = ArrayDeque<Pair<Int, Int>>()
+    val tasks = ArrayList<Task>()
     repeat(n) {
-
+        val (start, duration) = readLine()!!.trim().split(" ").map { it.toInt() }
+        tasks.add(Task(start, duration))
     }
+    tasks.sortBy { it.start }
+    val employees = PriorityQueue<Int>()
+
 }
+
+data class Task(
+    val start: Int,
+    val duration: Int
+)
