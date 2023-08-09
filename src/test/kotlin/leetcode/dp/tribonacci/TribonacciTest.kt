c41399b951c5e7bb924dbeff9d69a3e2846ebea2
aleksas <test@test.com>
2023-08-09 17:45:14 +0300

    Solve tribonacci in dynamic programming

---
+++ src/test/kotlin/leetcode/dp/tribonacci/TribonacciTest.kt
@@ -0,0 +1,11 @@
+package leetcode.dp.tribonacci
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class TribonacciTest {
+    @Test
+    fun testTribonacci() {
+        assertEquals(1389537, tribonacci(25))
+    }
+}
