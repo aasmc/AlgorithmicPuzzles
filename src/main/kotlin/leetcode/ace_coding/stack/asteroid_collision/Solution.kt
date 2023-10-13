4843e2dad861ce66cd093e8e6e6223b930a3bb84
Alex Alexeyev <aasmc@yandex.ru>
2023-10-13 13:25:45 +0300

    refactor leetcode solution of asteroid collision

--- src/main/kotlin/leetcode/ace_coding/stack/asteroid_collision/Solution.kt
+++ src/main/kotlin/leetcode/ace_coding/stack/asteroid_collision/Solution.kt
@@ -4,57 +4,34 @@ import kotlin.math.abs
 
 fun asteroidCollision(asteroids: IntArray): IntArray {
     val stack = ArrayDeque<Int>()
-    for (i in asteroids.indices) {
+    var i = 0
+    while (i < asteroids.size) {
         if (stack.isEmpty()) {
-            stack.addLast(asteroids[i])
+            stack.addLast(asteroids[i++])
         } else {
-            val current = asteroids[i]
-            val prev = stack.last()
-            // case 1: moving in the same direction
-            if ((current > 0 && prev > 0) || (current < 0 && prev < 0)) {
-                stack.addLast(current)
-            } else {
-                val absCurrent = abs(current)
-                val absPrev = abs(prev)
-                // case 2: moving towards each other and prev == current -> both explode
-                if (absCurrent == absPrev && prev > 0 && current < 0) {
-                    stack.removeLast()
-                    // case 3: moving towards each other and prev < current -> prev explodes
-                    // current moves further and collides with the next asteroid
-                } else if (absCurrent > absPrev && prev > 0 && current < 0) {
-                    stack.removeLast()
-                    if (lastWins(stack, current, absCurrent)) {
-                        stack.addLast(current)
+            while (i < asteroids.size && stack.isNotEmpty() && stack.last() > 0 && asteroids[i] < 0) {
+                val current = asteroids[i]
+                val prev = stack.last()
+                val diff = prev + current
+                when {
+                    diff > 0 -> {
+                        ++i
                     }
-                    // case4: moving away from each other
-                } else if (prev < 0 && current > 0) {
-                    stack.addLast(current)
+
+                    diff < 0 -> {
+                        stack.removeLast()
+                    }
+
+                    else -> {
+                        stack.removeLast()
+                        ++i
+                    }
                 }
+            }
+            if (i < asteroids.size) {
+                stack.addLast(asteroids[i++])
             }
         }
     }
-    val result = IntArray(stack.size)
-    for (i in stack.indices) {
-        result[i] = stack[i]
-    }
-    return result
-}
-
-private fun lastWins(stack: ArrayDeque<Int>, last: Int, lastAbs: Int): Boolean {
-    while (stack.isNotEmpty()) {
-        if (stack.last() > 0 && last > 0 || stack.last() < 0 && last < 0) {
-            return true
-        } else {
-            if (abs(stack.last()) == lastAbs) {
-                stack.removeLast()
-                return false
-            } else
-                if (abs(stack.last()) < lastAbs && stack.last() > 0 && last < 0) {
-                    stack.removeLast()
-                } else {
-                    return false
-                }
-        }
-    }
-    return true
+    return stack.toIntArray()
 }
--- src/test/kotlin/leetcode/ace_coding/stack/asteroid_collision/AsteroidCollisionTest.kt
+++ src/test/kotlin/leetcode/ace_coding/stack/asteroid_collision/AsteroidCollisionTest.kt
@@ -7,14 +7,14 @@ class AsteroidCollisionTest {
 
     @Test
     fun asteroidCollisionTest() {
-        val ex1 = intArrayOf(10)
-        assertTrue(ex1.contentEquals(asteroidCollision(intArrayOf(10, 2, -5))))
-        val ex2 = intArrayOf()
-        assertTrue(ex2.contentEquals(asteroidCollision(intArrayOf(8, -8))))
-        val ex3 = intArrayOf(5, 10)
-        assertTrue(ex3.contentEquals(asteroidCollision(intArrayOf(5, 10, -5))))
-        val ex4 = intArrayOf(-2, -1, 1, 2)
-        assertTrue(ex4.contentEquals(asteroidCollision(intArrayOf(-2,-1,1,2))))
+//        val ex1 = intArrayOf(10)
+//        assertTrue(ex1.contentEquals(asteroidCollision(intArrayOf(10, 2, -5))))
+//        val ex2 = intArrayOf()
+//        assertTrue(ex2.contentEquals(asteroidCollision(intArrayOf(8, -8))))
+//        val ex3 = intArrayOf(5, 10)
+//        assertTrue(ex3.contentEquals(asteroidCollision(intArrayOf(5, 10, -5))))
+//        val ex4 = intArrayOf(-2, -1, 1, 2)
+//        assertTrue(ex4.contentEquals(asteroidCollision(intArrayOf(-2,-1,1,2))))
         val ex5 = intArrayOf(-2, -2, -2)
         assertTrue(ex5.contentEquals(asteroidCollision(intArrayOf(-2, -2, 1, -2))))
         val ex6 = intArrayOf(-2)
