050d86a4ad53791eb433a6462831296411bfe19f
aleksas <test@test.com>
2023-10-16 11:08:14 +0300

    solve leetcode count good nodes binary tree

---
+++ src/main/kotlin/leetcode/ace_coding/binary_tree_dfs/count_good_nodes/CountGoodNodesSolution.kt
@@ -0,0 +1,26 @@
+package leetcode.ace_coding.binary_tree_dfs.count_good_nodes
+
+import leetcode.ace_coding.binary_tree_dfs.common.TreeNode
+
+class CountGoodNodesSolution {
+
+    fun goodNodes(root: TreeNode?): Int {
+        if (root == null) return 0
+        var count = 0
+        fun helper(node: TreeNode?, max: Int) {
+            if (node != null) {
+                var cMax = max
+                if (node.`val` >= max) {
+                    ++count
+                    cMax = node.`val`
+                }
+                helper(node.left, cMax)
+                helper(node.right, cMax)
+            }
+        }
+        helper(root, Int.MIN_VALUE)
+        return count
+    }
+
+
+}
