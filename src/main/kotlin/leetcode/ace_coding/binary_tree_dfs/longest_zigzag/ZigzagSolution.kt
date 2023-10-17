a1d1719a50e297c6a265206307969316a272e38c
aleksas <test@test.com>
2023-10-17 11:08:12 +0300

    solve leetcode longest zigzag

---
+++ src/main/kotlin/leetcode/ace_coding/binary_tree_dfs/longest_zigzag/ZigzagSolution.kt
@@ -0,0 +1,26 @@
+package leetcode.ace_coding.binary_tree_dfs.longest_zigzag
+
+import leetcode.ace_coding.binary_tree_dfs.common.TreeNode
+
+class ZigzagSolution {
+
+    fun longestZigZag(root: TreeNode?): Int {
+        var result = 0
+        fun dfs(node: TreeNode?, goLeft: Boolean, count: Int) {
+            if (node != null) {
+                result = maxOf(result, count)
+                if (goLeft) {
+                    dfs(node.left, false, count + 1)
+                    dfs(node.right, true, 1)
+                } else {
+                    dfs(node.left, false, 1)
+                    dfs(node.right, true, count + 1)
+                }
+            }
+        }
+        dfs(root, false, 0)
+        dfs(root, true, 0)
+        return result
+    }
+
+}
