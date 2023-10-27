ddc03a7bb35eeb86877f5c023b491a62cd08c5ff
aleksas <test@test.com>
2023-10-27 14:05:48 +0300

    solve leetcode monotonic stack dayly temperature (add comments)

--- src/main/kotlin/leetcode/ace_coding/monotonic_stack/daily_temp/DailyTempSolution.kt
+++ src/main/kotlin/leetcode/ace_coding/monotonic_stack/daily_temp/DailyTempSolution.kt
@@ -7,12 +7,22 @@ class DailyTempSolution {
 
     fun dailyTemperatures(temperatures: IntArray): IntArray {
         val res = IntArray(temperatures.size)
+        // список необработанных значений температур и их индексов в массиве temperatures,
+        // для которых мы еще не знаем когда наступит потепление.
         val stack = LinkedList<IntArray>()
+        // Проходимся по каждой температуре
         for ((curIdx, t) in temperatures.withIndex()) {
+            // если последнее значение необработанной температуры меньше текущего,
+            // значит мы нашли день, когда будет потепление - сохраним результат
+            // как разницу в индексах между текущим значением и необработанным
+            // и удалим необработаное значение, так как мы его только что обработали
+            // Это мы повторяем до тех пор, пока необработанные значения меньше текущего
             while (stack.isNotEmpty() && stack.peekLast()[1] < t) {
                 val (stackIndex, _) = stack.removeLast()
                 res[stackIndex] = curIdx - stackIndex
             }
+            // добавляем текущее значение в список необработанных, так как для него
+            // тоже надо найти день, когда будет потепление
             stack.addLast(intArrayOf(curIdx, t))
         }
         return res
