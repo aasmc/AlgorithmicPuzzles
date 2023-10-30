309fbcdc02bab239859ef45ec290e6fa3101cef2
aleksas <test@test.com>
2023-10-30 09:11:51 +0300

    solve leetcode monotonic stack stock spanner add comments

--- src/main/kotlin/leetcode/ace_coding/monotonic_stack/online_stock/OnlineStockSolution.kt
+++ src/main/kotlin/leetcode/ace_coding/monotonic_stack/online_stock/OnlineStockSolution.kt
@@ -4,12 +4,22 @@ import java.util.LinkedList
 
 class StockSpanner() {
 
+    /**
+     * Stores span of a stock to stock price.
+     * Prices are stored in monotonically decreasing order, i.e.
+     * if we pop all elements from the stack, they will be in increasing
+     * order.
+     */
     val stack = LinkedList<Pair<Int, Int>>()
     fun next(price: Int): Int {
+        // counts the number of days of the span
         var res = 1
+        // go down the stack until it is empty or you meet a price
+        // that is smaller than current price, and update the span
         while (stack.isNotEmpty() && stack.peekLast().second <= price) {
             res += stack.removeLast().first
         }
+        // add current span to price to the stack.
         stack.addLast(res to price)
         return res
     }
