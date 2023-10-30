2f60e617c1bbae8f8765cdc4c31fddaf5cf633a4
aleksas <test@test.com>
2023-10-30 11:40:02 +0300

    solve leetcode array string best time to buy sell stocks

---
+++ src/main/kotlin/leetcode/top_interview_150/array_string/best_time_buy_sell/StockBuySellSolution.kt
@@ -0,0 +1,16 @@
+package leetcode.top_interview_150.array_string.best_time_buy_sell
+
+class StockBuySellSolution {
+
+    fun maxProfit(prices: IntArray): Int {
+        var min = Int.MAX_VALUE
+        var profit = 0
+        for (price in prices) {
+            min = minOf(min, price)
+            val currentProfit = price - min
+            profit = maxOf(profit, currentProfit)
+        }
+        return profit
+    }
+
+}
