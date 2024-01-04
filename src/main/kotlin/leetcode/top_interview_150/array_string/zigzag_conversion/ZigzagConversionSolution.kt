a07c4c7418bf2856ed541145fed8fe775d1f13fa
aleksas <test@test.com>
2024-01-04 16:22:53 +0300

    solve leetcode 6. Zigzag Conversion

---
+++ src/main/kotlin/leetcode/top_interview_150/array_string/zigzag_conversion/ZigzagConversionSolution.kt
@@ -0,0 +1,25 @@
+package leetcode.top_interview_150.array_string.zigzag_conversion
+
+class ZigzagConversionSolution {
+
+    fun convert(s: String, numRows: Int): String {
+        if (numRows == 1) return s
+        val sb = StringBuilder()
+        for (row in 0 until numRows) {
+            val baseStep = (numRows - 1) * 2
+            for (i in row until s.length step baseStep) {
+                sb.append(s[i])
+                val interimStep = i + baseStep - 2 * row
+                if (isIntermediateStep(row, numRows) && interimStep < s.length) {
+                    sb.append(s[interimStep])
+                }
+            }
+        }
+        return sb.toString()
+    }
+
+    private fun isIntermediateStep(row: Int, numRows: Int) =
+        row > 0 && row < numRows - 1
+
+
+}
