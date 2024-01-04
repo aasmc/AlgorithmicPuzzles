0e7ddd439d4dbe49784e1241f298975de5041872
aleksas <test@test.com>
2024-01-04 09:30:07 +0300

    solve leetcode candies and trapping water

---
+++ src/main/kotlin/leetcode/top_interview_150/array_string/candy/CandySolution.kt
@@ -0,0 +1,29 @@
+package leetcode.top_interview_150.array_string.candy
+
+class CandySolution {
+
+    fun candy(ratings: IntArray): Int {
+        val candies = IntArray(ratings.size) { 1 }
+        // this loop ensures that a particular guy has more candies than the one to the
+        // left of him, if this guy's rating is greater than the rating of the guy to
+        // the left of him
+        for (i in 1 until ratings.size) {
+            if (ratings[i - 1] < ratings[i]) {
+                candies[i] = candies[i - 1] + 1
+            }
+        }
+        // this loop ensures that a particular guy has more candies than the one to the
+        // right of him, if this guy's rating is greater than the rating of the guy to
+        // the right of him, but in this case we must consider the case when this
+        // guy already has more candies than the guy to the right of him (this may happen
+        // on the first pass in the loop above), in this case we don't change the number
+        // of candies of this guy
+        for (i in ratings.lastIndex - 1 downTo 0) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = maxOf(candies[i], candies[i + 1] + 1)
+            }
+        }
+        return candies.sumOf { it }
+    }
+
+}
---
+++ src/main/kotlin/leetcode/top_interview_150/array_string/trapping_water/TrappingWaterSolution.kt
@@ -0,0 +1,41 @@
+package leetcode.top_interview_150.array_string.trapping_water
+
+class TrappingWaterSolution {
+
+    fun trap(height: IntArray): Int {
+        var left = 0
+        var right = height.lastIndex
+        var maxLeft = height.first()
+        var maxRight = height.last()
+        var result = 0
+        while (left <= right) {
+            if (maxLeft <= maxRight) {
+                result += maxOf(maxLeft - height[left], 0)
+                maxLeft = maxOf(maxLeft, height[left])
+                ++left
+            } else {
+                result += maxOf(maxRight - height[right], 0)
+                maxRight = maxOf(maxRight, height[right])
+                --right
+            }
+        }
+        return result
+    }
+
+    fun trapWithAdditionalMemory(height: IntArray): Int {
+        val leftMax = IntArray(height.size) { 0 }
+        for (i in 1 until leftMax.size) {
+            leftMax[i] = maxOf(leftMax[i - 1], height[i - 1])
+        }
+        val rightMax = IntArray(height.size) { 0 }
+        for (i in rightMax.lastIndex - 1 downTo 0) {
+            rightMax[i] = maxOf(rightMax[i + 1], height[i + 1])
+        }
+        var res = 0
+        for (i in height.indices) {
+            res += maxOf(minOf(leftMax[i], rightMax[i]) - height[i], 0)
+        }
+        return res
+    }
+
+}
