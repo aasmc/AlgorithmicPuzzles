d3ed43a71a3d4843c62f45cabf1b234755e8597c
Alex Alexeyev <aasmc@yandex.ru>
2024-01-16 01:52:53 +0300

    WIP add kotlin solution with sequences. Still not passes by TL.

--- src/main/kotlin/yandex_algo_training/internship/inversions/solution.kt
+++ src/main/kotlin/yandex_algo_training/internship/inversions/solution.kt
@@ -2,47 +2,86 @@ package yandex_algo_training.internship.inversions
 
 fun main() {
     val n = readln().toInt()
-    val numbers = readln().trim().split("\\s+".toRegex()).map { it.toInt() }.toIntArray()
-    val result: Pair<Int, Int> = countAverageInversions(numbers)
+    val numbers = readln().trim().split(" ").map { it.toInt() }.toMutableList()
+    val result: Pair<Long, Long> = countInversionAverageViaSequence(numbers)
     println("${result.first}/${result.second}")
 }
 
 
-private fun countAverageInversions(numbers: IntArray): Pair<Int, Int> {
-    var totalInversions = 0
-    var totalReplacements = 0
-    for (i in numbers.indices) {
-        for (j in i + 1 until numbers.size) {
-            val copy = IntArray(numbers.size)
-            System.arraycopy(numbers, 0, copy, 0, numbers.size)
-            val tmp = copy[i]
-            copy[i] = copy[j]
-            copy[j] = tmp
-            totalInversions += countInversions(copy)
-            ++totalReplacements
-        }
+private fun countInversionAverageViaSequence(numbers: MutableList<Int>): Pair<Long, Long> {
+    var totalInv = 0L
+    var totalPerm = 0L
+    val temp = IntArray(numbers.size)
+    permutationSequence(numbers).forEach { permutation ->
+        totalInv += countInversionsViaMergeSort(permutation.toMutableList(), temp, permutation.size)
+        ++totalPerm
     }
-    return reduce(totalInversions, totalReplacements)
+    return reduce(totalInv, totalPerm)
 }
 
-private fun reduce(left: Int, right: Int): Pair<Int, Int> {
+
+private fun permutationSequence(numbers: MutableList<Int>): Sequence<List<Int>> = sequence {
+    for (i in numbers.indices) {
+        for (j in i + 1 until numbers.size) {
+            swap(numbers, i, j)
+            yield(numbers)
+            swap(numbers, i, j)
+        }
+    }
+}
+
+private fun swap(arr: MutableList<Int>, l: Int, r: Int) {
+    val tmp = arr[l]
+    arr[l] = arr[r]
+    arr[r] = tmp
+}
+
+private fun reduce(left: Long, right: Long): Pair<Long, Long> {
     val gcd = gcd(left, right)
     return left / gcd to right / gcd
 }
 
-private tailrec fun gcd(x: Int, y: Int): Int {
-    if (y == 0) return x
+private tailrec fun gcd(x: Long, y: Long): Long {
+    if (y == 0L) return x
     return gcd(y, x % y)
 }
 
-private fun countInversions(numbers: IntArray): Int {
-    var inversions = 0
-    for (i in numbers.indices) {
-        for (j in i + 1 until numbers.size) {
-            if (numbers[i] > numbers[j]) {
-                ++inversions
-            }
+private fun mergeAndCount(arr: MutableList<Int>, temp: IntArray, left: Int, mid: Int, right: Int): Long {
+    var i = left
+    var j = mid + 1
+    var k = left
+    var invCount = 0L
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++]
+        } else {
+            temp[k++] = arr[j++]
+            invCount += (mid - i + 1)
         }
     }
-    return inversions
+    while (i <= mid) {
+        temp[k++] = arr[i++]
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++]
+    }
+    for(n in left..right) {
+        arr[n] = temp[n]
+    }
+    return invCount
+}
+
+private fun mergeSortAndCount(arr: MutableList<Int>, temp: IntArray, l: Int, r: Int): Long {
+    var invCount = 0L
+    if (l < r) {
+        val m = l + (r - l) / 2
+        invCount += mergeSortAndCount(arr, temp, l, m)
+        invCount += mergeSortAndCount(arr, temp, m + 1, r)
+        invCount += mergeAndCount(arr, temp, l, m, r)
+    }
+    return invCount
+}
+
+private fun countInversionsViaMergeSort(arr: MutableList<Int>, temp: IntArray, size: Int): Long {
+    return mergeSortAndCount(arr, temp, 0, size - 1)
 }
