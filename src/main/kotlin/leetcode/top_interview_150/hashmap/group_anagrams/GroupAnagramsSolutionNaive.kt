dda83744ede98d90be7aa97ac996b66174b86e2c
aleksas <aasmc@yandex.ru>
2024-02-05 14:46:55 +0300

    solve leetcode 49. Group Anagrams (kotlin style)

---
+++ src/main/kotlin/leetcode/top_interview_150/hashmap/group_anagrams/GroupAnagramsSolutionNaive.kt
@@ -0,0 +1,38 @@
+package leetcode.top_interview_150.hashmap.group_anagrams
+
+class GroupAnagramsSolutionNaive {
+
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val map = hashMapOf<String, MutableList<String>>()
+        strs.forEach { str ->
+            if (map.isEmpty()) {
+                map[str] = arrayListOf(str)
+            } else {
+                var isNew = true
+                for (key in map.keys) {
+                    if (isAnagram(key, str)) {
+                        map[key]!!.add(str)
+                        isNew = false
+                        break
+                    }
+                }
+                if(isNew) {
+                    map[str] = arrayListOf(str)
+                }
+            }
+        }
+        return map.values.toList()
+    }
+
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        val map = hashMapOf<Char, Int>()
+        s.forEach { ch -> map.merge(ch, 1, Int::plus) }
+        for (ch in t) {
+            map.merge(ch, -1, Int::plus)
+            if (map[ch]!! <0) return false
+        }
+        return true
+    }
+
+}
