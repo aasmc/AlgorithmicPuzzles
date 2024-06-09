5115600289c6149d5fe93776759ccf639774feec
Alex Alexeyev <aasmc@yandex.ru>
2024-06-09 19:00:24 +0300

    solve majority element

--- src/main/kotlin/leetcode/top_interview_150/array_string/majority_element/MajorityElementSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/array_string/majority_element/MajorityElementSolution.kt
@@ -3,29 +3,19 @@ package leetcode.top_interview_150.array_string.majority_element
 class MajorityElementSolution {
 
     fun majorityElement(nums: IntArray): Int {
-        var resIdx = 0
-        var cnt = 1
-        for (i in 1 until nums.size) {
-            if (nums[i] == nums[resIdx]) {
-                ++cnt
+        var count = 0
+        var candidate = 0
+        for (num in nums) {
+            if (count == 0) {
+                candidate = num
+            }
+            if (num == candidate) {
+                ++count
             } else {
-                --cnt
-            }
-            if (cnt == 0) {
-                cnt = 1
-                resIdx = i
+                --count
             }
         }
-        cnt = 0
-        nums.forEach { n ->
-            if (nums[resIdx] == n) {
-                ++cnt
-            }
-        }
-        if (cnt > nums.size / 2) {
-            return nums[resIdx]
-        }
-        return -1
+        return candidate
     }
 
 }
