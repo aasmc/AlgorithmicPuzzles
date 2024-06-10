3b1962562dd14ea8e919c90409b04a28e45b8782
aleksas <aasmc@yandex.ru>
2024-06-10 15:47:01 +0300

    add hIndex solution using counting sort

--- src/main/kotlin/leetcode/top_interview_150/array_string/h_index/solution.kt
+++ src/main/kotlin/leetcode/top_interview_150/array_string/h_index/solution.kt
@@ -2,13 +2,38 @@ package leetcode.top_interview_150.array_string.h_index
 
 class HIndexSolution {
 
-    fun hIndex(citations: IntArray): Int {
+    fun hIndex2(citations: IntArray): Int {
         citations.sort()
         val size = citations.size
         for (i in citations.indices) {
             val curNumber = citations[i]
             val left = size - i
             if (curNumber >= left) return left
+        }
+        return citations[0]
+    }
+
+    fun hIndex(citations: IntArray): Int {
+        if (citations.isEmpty()) return 0
+        val size = citations.size
+        val buckets = IntArray(size + 1)
+        citations.forEach { cit ->
+            if (cit > size) {
+                buckets[size]++
+            } else {
+                buckets[cit]++
+            }
+        }
+        var idx = 0
+        buckets.forEachIndexed { number, count ->
+            for (i in 0 until count) {
+                citations[idx++] = number
+            }
+        }
+        for (i in citations.indices) {
+            val current = citations[i]
+            val left = size - i
+            if (current >= left) return left
         }
         return citations[0]
     }
