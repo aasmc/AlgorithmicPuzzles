4ebe5f015cffe7ef53cb312409c9d4e0bbffe99e
aleksas <aasmc@yandex.ru>
2024-06-10 17:01:59 +0300

    refactor gas station solution

--- src/main/kotlin/leetcode/top_interview_150/array_string/gas_stations/GasStationsSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/array_string/gas_stations/GasStationsSolution.kt
@@ -3,20 +3,32 @@ package leetcode.top_interview_150.array_string.gas_stations
 class GasStationsSolution {
 
     fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
-        if (gas.sumOf { it } < cost.sumOf { it }) {
-            return -1
-        }
+        // holds total amount of gas spent during the trip
         var total = 0
-        var start = 0
-        for (idx in gas.indices) {
-            val diff = gas[idx] - cost[idx]
-            total += diff
-            if (total < 0) {
-                total = 0
-                start = idx + 1
+        // holds the amount of gas spent during the trip when we can really move, i.e.
+        // we have enough gas to spend during the next move to the gas station
+        var sum = 0
+        // keeps track of the position, where we started to move without going into
+        // negative gas tank
+        var position = 0
+        // try every gas station
+        for (gasStation in gas.indices) {
+            // sum up how much gas we need to spend to get to the next station
+            // taking into account the amount of gas we can get at this gas station
+            sum += gas[gasStation] - cost[gasStation]
+            // if we can't reach the next station
+            if (sum < 0) {
+                // first accumulate the total gas spendings
+                total += sum
+                // reset the sum and try from the next position
+                sum = 0
+                position = gasStation + 1
             }
         }
-        return start
+        total += sum
+        // total >= 0 means we could travel around the circle from that position
+        // without going into negative gas tank
+        return if (total >= 0) position else -1
     }
 
 
--- src/test/kotlin/leetcode/top_interview_150/array_string/gas_stations/GasStationsSolutionTest.kt
+++ src/test/kotlin/leetcode/top_interview_150/array_string/gas_stations/GasStationsSolutionTest.kt
@@ -11,6 +11,7 @@ class GasStationsSolutionTest {
     fun testCorrect() {
         assertEquals(3, sut.canCompleteCircuit(intArrayOf(1,2,3,4,5), intArrayOf(3,4,5,1,2)))
         assertEquals(-1, sut.canCompleteCircuit(intArrayOf(2,3,4), intArrayOf(3,4,3)))
+        assertEquals(-1, sut.canCompleteCircuit(intArrayOf(4,5,2,6,5,3), intArrayOf(3,2,7,3,2,9)))
     }
 
 }
