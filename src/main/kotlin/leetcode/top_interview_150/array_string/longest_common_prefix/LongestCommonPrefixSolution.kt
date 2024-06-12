4acdc23669bc1a858e34f9d2cb5c49f7ce673a2a
Alex Alexeyev <aasmc@yandex.ru>
2024-06-12 21:55:28 +0300

    add another solution to longest common prefix problem

--- src/main/kotlin/leetcode/top_interview_150/array_string/longest_common_prefix/LongestCommonPrefixSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/array_string/longest_common_prefix/LongestCommonPrefixSolution.kt
@@ -3,6 +3,28 @@ package leetcode.top_interview_150.array_string.longest_common_prefix
 class LongestCommonPrefixSolution {
 
     fun longestCommonPrefix(strs: Array<String>): String {
+        val res = StringBuilder()
+        val firstString = strs[0]
+        // for every index in the first string
+        for (i in firstString.indices) {
+            // for every string in the input array
+            for (j in 1 until strs.size) {
+                val currentString = strs[j]
+                // if current index is out of bounds for the current string
+                // or current string's character is not equal to the character
+                // at the same position in the first string
+                if (i == currentString.length || currentString[i] != firstString[i]) {
+                    // return what we have accumulated so far
+                    return res.toString()
+                }
+            }
+            // don't forget to add the current character to the accumulator
+            res.append(firstString[i])
+        }
+        return res.toString()
+    }
+
+    fun longestCommonPrefix2(strs: Array<String>): String {
         strs.sort()
         var idx = 0
         val first = strs.first()
