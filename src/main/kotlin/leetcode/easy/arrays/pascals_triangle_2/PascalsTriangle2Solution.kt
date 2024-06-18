e50b889cc43fc80af8f9fbb955b32e5a46bd461e
Alex Alexeyev <aasmc@yandex.ru>
2024-06-18 12:18:44 +0300

    solve leetcode  119. Pascal's Triangle II

---
+++ src/main/kotlin/leetcode/easy/arrays/pascals_triangle_2/PascalsTriangle2Solution.kt
@@ -0,0 +1,21 @@
+package leetcode.easy.arrays.pascals_triangle_2
+
+class PascalsTriangle2Solution {
+
+    fun getRow(rowIndex: Int): List<Int> {
+        var result = mutableListOf<Int>()
+        repeat(rowIndex + 1) {
+            val currentRow = mutableListOf<Int>()
+            for(i in 0..result.size) {
+                if (i == 0 || i == result.size) {
+                    currentRow.add(1)
+                } else {
+                    currentRow.add(result[i - 1] + result[i])
+                }
+            }
+            result = currentRow
+        }
+        return result
+    }
+
+}
