aa409815f354a079bdd77b47a1c05cd3296589b0
aleksas <aasmc@yandex.ru>
2024-06-22 15:29:09 +0300

    fix leetcode 11. Container With Most Water

--- src/main/kotlin/leetcode/top_interview_150/two_pointers/water_container/WaterContainerSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/two_pointers/water_container/WaterContainerSolution.kt
@@ -3,12 +3,12 @@ package leetcode.top_interview_150.two_pointers.water_container
 class WaterContainerSolution {
 
     fun maxArea(height: IntArray): Int {
-        if (height.size == 1) return 1
+        if (height.size == 1) return 0
 
         var max = Int.MIN_VALUE
         var start = 0
         var end = height.lastIndex
-        while (start <= end) {
+        while (start < end) {
             val diff = end - start
             val minHeight = minOf(height[start], height[end])
             val square = diff * minHeight
