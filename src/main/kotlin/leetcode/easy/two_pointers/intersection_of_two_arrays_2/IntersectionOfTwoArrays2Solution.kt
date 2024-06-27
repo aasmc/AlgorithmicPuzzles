9ca46baa1e465560093f60160eff85f0c9fb2631
aleksas <aasmc@yandex.ru>
2024-06-27 15:08:17 +0300

    solve leetcode 350. Intersection of Two Arrays II another way

--- src/main/kotlin/leetcode/easy/two_pointers/intersection_of_two_arrays_2/IntersectionOfTwoArrays2Solution.kt
+++ src/main/kotlin/leetcode/easy/two_pointers/intersection_of_two_arrays_2/IntersectionOfTwoArrays2Solution.kt
@@ -3,6 +3,36 @@ package leetcode.easy.two_pointers.intersection_of_two_arrays_2
 class IntersectionOfTwoArrays2Solution {
 
     fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+        nums1.sort()
+        nums2.sort()
+
+        var nIdx = 0
+        var mIdx = 0
+        val result = arrayListOf<Int>()
+        while (nIdx < nums1.size && mIdx < nums2.size) {
+            while (nIdx < nums1.size && nums1[nIdx] < nums2[mIdx]) {
+                ++nIdx
+            }
+            if (nIdx >= nums1.size) {
+                break
+            }
+            while (mIdx < nums2.size && nums2[mIdx] < nums1[nIdx]) {
+                ++mIdx
+            }
+            if (mIdx >= nums2.size) {
+                break
+            }
+            if (nums1[nIdx] == nums2[mIdx]) {
+                result.add(nums1[nIdx])
+                ++nIdx
+                ++mIdx
+            }
+        }
+        return result.toIntArray()
+    }
+
+
+    fun intersect2(nums1: IntArray, nums2: IntArray): IntArray {
         val nMap = hashMapOf<Int, Pair<Int, Boolean>>()
         val mMap = hashMapOf<Int, Int>()
         nums1.forEach { num ->
