90deb6cf1ce20debb5d866453a5a997c1150bd26
Alex Alexeyev <aasmc@yandex.ru>
2024-07-02 17:25:11 +0300

    solve leetcode 101. Symmetric Tree

---
+++ src/main/kotlin/leetcode/top_interview_150/binary_search_tree/symmetric_tree/SymmetricTreeSolution.kt
@@ -0,0 +1,22 @@
+package leetcode.top_interview_150.binary_search_tree.symmetric_tree
+
+class SymmetricTreeSolution {
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun isSymmetric(root: TreeNode?): Boolean {
+        if (root == null) return false
+        fun dfs(l: TreeNode?, r: TreeNode?): Boolean {
+            if (l == null && r == null) return true
+            if (l == null || r == null) return false
+            return l.`val` == r.`val` &&
+                    dfs(l.left, r.right) &&
+                    dfs(l.right, r.left)
+        }
+        return dfs(root.left, root.right)
+    }
+
+}
