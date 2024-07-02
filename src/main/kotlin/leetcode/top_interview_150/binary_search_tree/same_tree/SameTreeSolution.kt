0f580934b615961ff9cb5ac1b3fa043266f4031c
Alex Alexeyev <aasmc@yandex.ru>
2024-07-02 16:30:09 +0300

    solve leetcode 100. Same Tree

---
+++ src/main/kotlin/leetcode/top_interview_150/binary_search_tree/same_tree/SameTreeSolution.kt
@@ -0,0 +1,17 @@
+package leetcode.top_interview_150.binary_search_tree.same_tree
+
+class SameTreeSolution {
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if (p == null && q == null) return true
+        return (p?.`val` == q?.`val`) &&
+                isSameTree(p?.left, q?.left) &&
+                isSameTree(p?.right, q?.right)
+    }
+
+}
