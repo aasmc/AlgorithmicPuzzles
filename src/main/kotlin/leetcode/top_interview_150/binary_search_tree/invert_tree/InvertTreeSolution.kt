7e3e5095ab415647bf0e61f71d484fc84e01a431
Alex Alexeyev <aasmc@yandex.ru>
2024-07-02 17:00:14 +0300

    solve leetcode 226. Invert Binary Tree

---
+++ src/main/kotlin/leetcode/top_interview_150/binary_search_tree/invert_tree/InvertTreeSolution.kt
@@ -0,0 +1,21 @@
+package leetcode.top_interview_150.binary_search_tree.invert_tree
+
+class InvertTreeSolution {
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root != null) {
+            val tmp = root.left
+            root.left = root.right
+            root.right = tmp
+            invertTree(root.left)
+            invertTree(root.right)
+        }
+        return root
+    }
+
+}
