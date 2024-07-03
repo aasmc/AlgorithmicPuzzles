f9a71d1182cf06913c87801eb9fb1230879fd436
Alex Alexeyev <aasmc@yandex.ru>
2024-07-03 19:06:51 +0300

    solve leetcode 222. Count Complete Tree Nodes

---
+++ src/main/kotlin/leetcode/top_interview_150/binary_search_tree/count_complete_tree_nodes/CountCompleteTreeNodesSolution.kt
@@ -0,0 +1,15 @@
+package leetcode.top_interview_150.binary_search_tree.count_complete_tree_nodes
+
+class CountCompleteTreeNodesSolution {
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun countNodes(root: TreeNode?): Int {
+        if (root == null) return 0
+        return countNodes(root.left) + countNodes(root.right) + 1
+    }
+
+}
