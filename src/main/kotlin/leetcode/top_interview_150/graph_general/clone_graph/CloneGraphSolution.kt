106d0ad21cc52d0b64771e860522fc6a39535c11
aleksas <aasmc@yandex.ru>
2024-07-10 15:22:22 +0300

    add description for leetcode 133. Clone Graph

---
+++ src/main/kotlin/leetcode/top_interview_150/graph_general/clone_graph/CloneGraphSolution.kt
@@ -0,0 +1,12 @@
+package leetcode.top_interview_150.graph_general.clone_graph
+
+class CloneGraphSolution {
+
+    class Node(var `val`: Int) {
+        var neighbors: ArrayList<Node?> = ArrayList<Node?>()
+    }
+
+    fun cloneGraph(node: Node?): Node? {
+        TODO()
+    }
+}
