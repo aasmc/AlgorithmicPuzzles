2e0c356c6fae9b4241ef758998a8df3cce7b41db
Alex Alexeyev <aasmc@yandex.ru>
2024-07-15 19:50:47 +0300

    solve leetcode 547. Number of Provinces using UnionFind

--- src/main/kotlin/leetcode/medium/graph/general/number_of_provinces/NumberOfProvincesSolution.kt
+++ src/main/kotlin/leetcode/medium/graph/general/number_of_provinces/NumberOfProvincesSolution.kt
@@ -2,7 +2,76 @@ package leetcode.medium.graph.general.number_of_provinces
 
 class NumberOfProvincesSolution {
 
+    class UnionFind(
+        private val size: Int
+    ) {
+        private var numComponents = size
+
+        private val componentsSizes = IntArray(size) { 1 }
+        private val ids = IntArray(size){ it }
+
+        fun find(p: Int): Int {
+            var root = p
+            while (root != ids[root]) {
+                root = ids[root]
+            }
+            var pp = p
+            while (pp != root) {
+                val next = ids[pp]
+                ids[pp] = root
+                pp = next
+            }
+            return root
+        }
+
+        fun connected(p: Int, q: Int): Boolean {
+            return find(p) == find(q)
+        }
+
+        fun components(): Int {
+            return numComponents
+        }
+
+        fun componentSize(p: Int): Int {
+            val root = find(p)
+            return componentsSizes[root]
+        }
+
+        fun unify(p: Int, q: Int) {
+            if (connected(p, q)) return
+            val rootOfP = find(p)
+            val rootOfQ = find(q)
+
+            if (componentsSizes[rootOfP] < componentsSizes[rootOfQ]) {
+                componentsSizes[rootOfQ] += componentsSizes[rootOfP]
+                ids[rootOfP] = rootOfQ
+                componentsSizes[rootOfP] = 0
+            } else {
+                componentsSizes[rootOfP] += componentsSizes[rootOfQ]
+                ids[rootOfQ] = rootOfP
+                componentsSizes[rootOfQ] = 0
+            }
+            --numComponents
+        }
+    }
+
     fun findCircleNum(isConnected: Array<IntArray>): Int {
+        val n = isConnected.size
+        val unionFind = UnionFind(n)
+
+        for (i in 0 until n) {
+            for (j in 0 until n) {
+                if (isConnected[i][j] == 1) {
+                    val iRoot = unionFind.find(i)
+                    val jRoot = unionFind.find(j)
+                    unionFind.unify(iRoot, jRoot)
+                }
+            }
+        }
+        return unionFind.components()
+    }
+
+    fun findCircleNum2(isConnected: Array<IntArray>): Int {
         val n = isConnected.size
         val visited = BooleanArray(n)
         fun dfs(vertex: Int) {
