136d6e19b2976a5f4942727ff620b2d7f5f33c6f
Alex Alexeyev <aasmc@yandex.ru>
2024-07-24 17:50:23 +0300

    solve leetcode 46. Permutations add another solution

--- src/main/kotlin/leetcode/top_interview_150/backtracking/permutations/PermutationsSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/backtracking/permutations/PermutationsSolution.kt
@@ -4,6 +4,28 @@ class PermutationsSolution {
 
     fun permute(nums: IntArray): List<List<Int>> {
         val result = mutableListOf<List<Int>>()
+        fun backtrack(used: MutableSet<Int>) {
+            if (nums.size == used.size) {
+                result.add(used.toList())
+                return
+            }
+
+            for (num in nums) {
+                if (num in used) continue
+                used.add(num)
+                backtrack(used)
+                used.remove(num)
+            }
+        }
+        backtrack(mutableSetOf<Int>())
+
+        return result
+    }
+
+
+
+    fun permute2(nums: IntArray): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
         val visited = hashSetOf<Int>()
         fun backTrack(currentIdx: Int, permutation: List<Int>) {
             if (permutation.size == nums.size) {
