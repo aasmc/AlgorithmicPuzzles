6622e4ff81585841b225225328a04bb09419d1b8
Alex Alexeyev <aasmc@yandex.ru>
2024-07-24 16:58:44 +0300

    solve leetcode 77. Combinations add another solution

--- src/main/kotlin/leetcode/top_interview_150/backtracking/combinations/CombinationsSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/backtracking/combinations/CombinationsSolution.kt
@@ -1,9 +1,29 @@
 package leetcode.top_interview_150.backtracking.combinations
 
+import java.util.LinkedList
+
 class CombinationsSolution {
 
     fun combine(n: Int, k: Int): List<List<Int>> {
 
+        val result = mutableListOf<List<Int>>()
+        fun backTrack(curNum: Int, combination: MutableList<Int>) {
+            if (combination.size == k) {
+                result.add(combination.toList())
+                return
+            }
+            for (i in curNum..n) {
+                combination.add(i)
+                backTrack(i + 1, combination)
+                combination.removeLast()
+            }
+        }
+        backTrack(1, mutableListOf())
+
+        return result
+    }
+
+    fun combine2(n: Int, k: Int): List<List<Int>> {
         val result = mutableListOf<List<Int>>()
         fun backTrack(curNum: Int, combination: List<Int>) {
             if (combination.size == k) {
@@ -15,7 +35,6 @@ class CombinationsSolution {
             }
         }
         backTrack(1, listOf())
-
         return result
     }
 
