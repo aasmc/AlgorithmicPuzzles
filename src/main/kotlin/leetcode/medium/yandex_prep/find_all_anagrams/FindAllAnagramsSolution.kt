0bfc7e335d714af39e76e48fc47718f29fca658a
Alex Alexeyev <aasmc@yandex.ru>
2024-07-30 21:40:09 +0300

    update leetcode 438. Find All Anagrams in a String

--- src/main/kotlin/leetcode/medium/yandex_prep/find_all_anagrams/FindAllAnagramsSolution.kt
+++ src/main/kotlin/leetcode/medium/yandex_prep/find_all_anagrams/FindAllAnagramsSolution.kt
@@ -6,10 +6,13 @@ class FindAllAnagramsSolution {
         if (p.length > s.length) return emptyList()
 
         val pCharToCount = hashMapOf<Char, Int>()
-        p.forEach { ch -> pCharToCount.merge(ch, 1, Int::plus) }
+        val sCharToCount = hashMapOf<Char, Int>()
+        for (i in p.indices) {
+            pCharToCount.merge(p[i], 1, Int::plus)
+            sCharToCount.merge(s[i], 1, Int::plus)
+        }
 
         val result = mutableListOf<Int>()
-        val sCharToCount = hashMapOf<Char, Int>()
 
         fun areFreqsEqual(): Boolean {
             for((pChar, pCount) in pCharToCount) {
@@ -22,14 +25,9 @@ class FindAllAnagramsSolution {
         }
 
         var start = 0
-        while (start < p.length) {
-            sCharToCount.merge(s[start++], 1, Int::plus)
-        }
-        start = 0
         if (areFreqsEqual()) {
             result.add(start)
         }
-
         for (right in p.length until s.length) {
             sCharToCount.merge(s[start++], -1, Int::plus)
             sCharToCount.merge(s[right], 1, Int::plus)
