8d3c5fafcfbcb3df877a41594ef4f519a2f7a42e
Alex Alexeyev <aasmc@yandex.ru>
2024-07-31 23:34:56 +0300

    add another solution to leetcode 2. Add Two Numbers

--- src/main/kotlin/leetcode/medium/yandex_prep/add_two_numbers/AddTwoNumbersSolution.kt
+++ src/main/kotlin/leetcode/medium/yandex_prep/add_two_numbers/AddTwoNumbersSolution.kt
@@ -10,6 +10,30 @@ class AddTwoNumbersSolution {
         if (l1 == null) return l2
         if (l2 == null) return l1
 
+        val dummy = ListNode(0)
+        var carry = 0
+        var current = dummy
+        var l1Cur = l1
+        var l2Cur = l2
+        while (l1Cur != null || l2Cur != null || carry != 0) {
+            val v1 = l1Cur?.`val` ?: 0
+            val v2 = l2Cur?.`val` ?: 0
+            var value = v1 + v2 + carry
+            carry = value / 10
+            value %= 10
+            current.next = ListNode(value)
+            current = current.next!!
+
+            l1Cur = l1Cur?.next
+            l2Cur = l2Cur?.next
+        }
+        return dummy.next
+    }
+
+    fun addTwoNumbers2(l1: ListNode?, l2: ListNode?): ListNode? {
+        if (l1 == null) return l2
+        if (l2 == null) return l1
+
         fun addWithCarry(node: ListNode, newNode: ListNode, carry: Int) {
             if (carry == 0) {
                 var cur: ListNode? = node
