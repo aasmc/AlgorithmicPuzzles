75f9dfec07e9738d65718d3935c936dc431187a1
Alex Alexeyev <aasmc@yandex.ru>
2024-08-03 20:56:39 +0300

    solve leetcode 279. Perfect Squares

---
+++ src/main/kotlin/leetcode/medium/yandex_prep/perfert_squares/PerfectSquaresSolution.kt
@@ -0,0 +1,20 @@
+package leetcode.medium.yandex_prep.perfert_squares
+
+class PerfectSquaresSolution {
+
+    fun numSquares(n: Int): Int {
+        val dp = IntArray(n + 1) { n }
+        dp[0] = 0
+
+        for(target in 1..n) {
+            for (s in 1..target) {
+                val square = s * s
+                if (target - square >= 0) {
+                    dp[target] = minOf(dp[target], dp[target - square] + 1)
+                }
+            }
+        }
+        return dp[n]
+    }
+
+}
