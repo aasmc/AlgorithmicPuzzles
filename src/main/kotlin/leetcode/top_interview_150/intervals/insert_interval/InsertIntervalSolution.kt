f7bdb396ae3ba5bcc9e310a3fb47e604a2f3b26c
aleksas <email@email.com>
2025-04-23 23:16:55 +0300

    make leetcode 57. Insert Interval more readable

--- src/main/kotlin/leetcode/top_interview_150/intervals/insert_interval/InsertIntervalSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/intervals/insert_interval/InsertIntervalSolution.kt
@@ -8,21 +8,40 @@ class InsertIntervalSolution {
         }
         val result = mutableListOf<IntArray>()
         for (i in intervals.indices) {
-            if (newInterval[1] < intervals[i][0]) {
+            val currentInterval = intervals[i]
+            if (newIntervalIsToTheLeftOfCurrent(newInterval, currentInterval)) {
                 result.add(newInterval)
-                for (j in i..intervals.lastIndex) {
-                    result.add(intervals[j])
-                }
+                addRemainingIntervals(i, intervals, result)
                 return result.toTypedArray()
-            } else if (newInterval[0] > intervals[i][1]) {
+            } else if (newIntervalIsToRightOfCurrent(newInterval, currentInterval)) {
                 result.add(intervals[i])
             } else {
-                newInterval[0] = minOf(newInterval[0], intervals[i][0])
-                newInterval[1] = maxOf(newInterval[1], intervals[i][1])
+                newInterval[0] = minOf(newInterval[0], currentInterval[0])
+                newInterval[1] = maxOf(newInterval[1], currentInterval[1])
             }
         }
         result.add(newInterval)
         return result.toTypedArray()
     }
+
+    private fun newIntervalIsToRightOfCurrent(
+        newInterval: IntArray,
+        currentInterval: IntArray
+    ) = newInterval[0] > currentInterval[1]
+
+    private fun addRemainingIntervals(
+        i: Int,
+        intervals: Array<IntArray>,
+        result: MutableList<IntArray>
+    ) {
+        for (j in i..intervals.lastIndex) {
+            result.add(intervals[j])
+        }
+    }
+
+    private fun newIntervalIsToTheLeftOfCurrent(
+        newInterval: IntArray,
+        currentInterval: IntArray
+    ) = newInterval[1] < currentInterval[0]
 
 }
