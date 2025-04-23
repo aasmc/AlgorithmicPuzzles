d14d9cecb3c06214f7de3fd5b61b74fb5d50f04f
aleksas <email@email.com>
2025-04-23 23:23:13 +0300

    make leetcode 452. Minimum Number of Arrows to Burst Balloons more readable

--- src/main/kotlin/leetcode/top_interview_150/intervals/arrows_balloons/ArrowsBalloonsSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/intervals/arrows_balloons/ArrowsBalloonsSolution.kt
@@ -6,16 +6,25 @@ class ArrowsBalloonsSolution {
         // sort by start of the interval
         points.sortBy { it[0] }
         var cnt = 1
-        var prevEnd = points[0][1]
+        var prevEnd = points[0].end()
         for (i in 1..points.lastIndex) {
-            if (points[i][0] > prevEnd) {
+            val currentInterval = points[i]
+            if (currentInterval.start() > prevEnd) {
                 ++cnt
-                prevEnd = points[i][1]
+                prevEnd = currentInterval.end()
             } else {
-                prevEnd = minOf(prevEnd, points[i][1])
+                prevEnd = minOf(prevEnd, currentInterval.end())
             }
         }
         return cnt
+    }
+
+    private fun IntArray.end(): Int {
+        return this[1]
+    }
+
+    private fun IntArray.start(): Int {
+        return this[0]
     }
 
 }
