98cf5b9a325c266272f7bbbd868626009b64f972
Alex Alexeyev <aasmc@yandex.ru>
2025-05-11 11:47:25 +0300

    add comments to leetcode 105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/kotlin/leetcode/top_interview_150/binary_tree_general/construct_from_inorder_preorder/ConstructFromInOrderPreorderSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/binary_tree_general/construct_from_inorder_preorder/ConstructFromInOrderPreorderSolution.kt
@@ -12,14 +12,14 @@ class ConstructFromInOrderPreorderSolution {
         // preorder = root -> left -> right
 
         // inorder: 9 -> 3 -> 15 -> 20 -> 7
-        // preorder: 3 -> 9 -> 20 -> 17 -> 7
+        // preorder: 3 -> 9 -> 20 -> 15 -> 7
         // output: 3 -> 9 -> 20 -> null -> null -> 15 -> 7
         // value of the root node is always going to be the first value from the
         // preorder array.
         // every value to the left of it (the first value from the preorder array)
         // in the inorder array will go to the left subtree, and every value
         // to the right of it will go to the right subtree.
-        fun calculate(rootIdx: Int, iFrom: Int, iTo: Int): TreeNode? {
+        fun construct(rootIdx: Int, iFrom: Int, iTo: Int): TreeNode? {
             if (iFrom > iTo) {
                 return null
             }
@@ -28,14 +28,27 @@ class ConstructFromInOrderPreorderSolution {
             while (preorder[rootIdx] != inorder[inOrderIdx]) {
                 inOrderIdx++
             }
-            // rootIdx + size + 1 - is the start of the right subtree nodes
-            // in the preorder array.
-            val size = inOrderIdx - iFrom
-            root.left = calculate(rootIdx + 1, iFrom, inOrderIdx - 1)
-            root.right = calculate(rootIdx + size + 1, inOrderIdx + 1, iTo)
+            // to construct left subtree, we need to move rootIdx by one to the right, since
+            // this will be the index of the root in the left subtree, iFrom stays the same,
+            // because all the values in inOrder traversal starting from that index go to
+            // the left subtree, but iTo is shifted to the inOrderIdx - 1, because we already
+            // processed the value at inOrderIdx (it is the current root), and all values to
+            // the left of it will also go to the left subtree
+            root.left = construct(rootIdx + 1, iFrom, inOrderIdx - 1)
+
+            // to construct the right subtree we need to shift the root index to the position
+            // where there is the root of the right subtree in the preOrder traversal. How can
+            // we do it? Well, we have current rootIdx, and we know exactly how many elements
+            // are going to be in the left subtree by looking at the indices
+            // in the inOrder traversal: inOrderIdx - iFrom + 1, therefore we need to shift
+            // the rootIdx to the right by that many elements. iFrom is also shifted
+            // to the next position in the inOrderTraversal, because we know that all
+            // the elements starting from it are in the right subtree. While iTo stays the same.
+            val rightSubtreeStartIdx = rootIdx + inOrderIdx - iFrom + 1
+            root.right = construct(rightSubtreeStartIdx, inOrderIdx + 1, iTo)
             return root
         }
-        return calculate(0, 0, inorder.lastIndex)
+        return construct(0, 0, inorder.lastIndex)
     }
 
 }
