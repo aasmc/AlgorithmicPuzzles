244259d5ae1ad715a8fc7bf1eb90fe570136fa04
Alex Alexeyev <aasmc@yandex.ru>
2025-05-11 13:49:19 +0300

    add efficient solution to leetcode 222. Count Complete Tree Nodes

--- src/main/kotlin/leetcode/top_interview_150/binary_tree_general/count_complete_tree_nodes/CountCompleteTreeNodesSolution.kt
+++ src/main/kotlin/leetcode/top_interview_150/binary_tree_general/count_complete_tree_nodes/CountCompleteTreeNodesSolution.kt
@@ -1,5 +1,7 @@
 package leetcode.top_interview_150.binary_tree_general.count_complete_tree_nodes
 
+import kotlin.math.pow
+
 class CountCompleteTreeNodesSolution {
 
     class TreeNode(var `val`: Int) {
@@ -10,6 +12,29 @@ class CountCompleteTreeNodesSolution {
     fun countNodes(root: TreeNode?): Int {
         if (root == null) return 0
         return countNodes(root.left) + countNodes(root.right) + 1
+    }
+
+    class Solution2 {
+        fun countNodes(root: TreeNode?): Int {
+            if (root == null) return 0
+            val leftHeight = leftHeight(root)
+            val rightHeight = rightHeight(root)
+            if (leftHeight == rightHeight) {
+                return 2.0.pow(leftHeight).toInt() - 1
+            } else {
+                return 1 + countNodes(root.left) + countNodes(root.right)
+            }
+        }
+
+        private fun leftHeight(node: TreeNode?): Int {
+            if (node == null) return 0
+            return 1 + leftHeight(node.left)
+        }
+
+        private fun rightHeight(node: TreeNode?): Int {
+            if (node == null) return 0
+            return 1 + rightHeight(node.right)
+        }
     }
 
 }
